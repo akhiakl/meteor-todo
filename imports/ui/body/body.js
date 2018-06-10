ddd034c4befa9fb837643cb00fa89968b3b827c2
akhilbit <akhil@bititude.com>
2018-06-10 12:33:33 +0530

    Created api for task collection and changed the html to show task lists

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -1,6 +1,14 @@
 import { Template } from 'meteor/templating';
+import { Tasks } from '../../api/tasks.js';
 import '../task/task.js';
 import './body.html';
 
+
+
+Template.body.helpers({
+    tasks() {
+        return Tasks.find({});
+    },
+});
 
 
