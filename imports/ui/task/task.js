0f50a839ba5cd30e77adf861e05a5c8c8e763606
akhilbit <akhil@bititude.com>
2018-06-10 22:38:40 +0530

    Task complete toggle

--- imports/ui/task/task.js
+++ imports/ui/task/task.js
@@ -1,2 +1,16 @@
 import { Template } from 'meteor/templating';
 import './task.html';
+import { Tasks } from '../../api/tasks';
+
+
+Template.task.events({
+    'click .delete'(event){
+        Tasks.remove(this._id)
+    }, //delete task
+    'click .checkbox-toggle'(event){
+        Tasks.update(this._id,{
+            $set: {checked: !this.checked}
+        })
+    } // toggle complete task
+
+})
